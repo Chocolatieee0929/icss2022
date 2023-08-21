a54f87911cbd0dd1d4fcaf62f8471a2b2122d67c
tracer-ics2022 <tracer@njuics.org>
2023-08-21 22:24:48 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:24:48 up 1 day, 21:08,  6 users,  load average: 0.25, 0.22, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -146,7 +146,7 @@ bool check_parentheses(int p,int q){
 // 寻找主运算符
 int mainToken(int p,int q){
     int mainindex = p;
-    int flag = 0;
+    int flag = 1;
     for(int i = p; i<q;i++){
       if(tokens[i].type == '+'||(tokens[i].type =='-'&& tokens[i-1].type!='-'&& tokens[i-1].type!='+'&&tokens[i-1].type!='*'&&tokens[i-1].type!='/')){
 	mainindex = i;
