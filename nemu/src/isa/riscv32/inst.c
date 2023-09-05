d7a44efcac381f9b198cc3b189eceb9e0c3eb8d3
tracer-ics2022 <tracer@njuics.org>
2023-09-05 21:33:06 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:06 up 2 days, 19:01, 13 users,  load average: 0.23, 0.27, 0.23

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -35,7 +35,7 @@ enum {
 #define immJ() do { \
        	*imm = SEXT((BITS(i, 31, 31) << 20| (BITS(i, 19,12) << 12) | (BITS(i,20,20) <<11)\
 			| (BITS(i, 30, 21) << 12)), 21);\
-	printf("imm %d",*imm);\
+	Log(ANSI_FG_CYAN "%#x\n" ANSI_NONE, *imm);\
 } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
