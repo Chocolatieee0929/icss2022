109bfb7721cc2105e95ce91fb22c96cd0e0acdb1
tracer-ics2022 <tracer@njuics.org>
2023-09-06 16:53:50 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:50 up 2 days, 20:54, 12 users,  load average: 0.41, 0.30, 0.30

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -58,8 +58,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_J: 		   immJ(); break;
     case TYPE_R: src1R(); src2R();         break;
     case TYPE_B: 
-		 src1R(); src2R();
-		 if(rs1==0) *src1=0; 
+		 src2R();
+		 if(rs1 != 0) src1R(); 
 		 immB(); break;
   }
 }
