42a02e396a39a3b897b60dff38765476e215766f
tracer-ics2022 <tracer@njuics.org>
2023-09-28 20:32:02 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:02 up 1 day, 0 min,  3 users,  load average: 0.42, 0.30, 0.24

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -7,11 +7,11 @@
                // it will be expanded as "x86/x86.h", "mips/mips32.h", ...
 
 #if defined(__ISA_X86__)
-# define nemu_trap(code) asm volatile ("int3" : :"a"(code))
+# define nemu_trap(code) asm volatile ("int3" : :"a"(code)) 
 #elif defined(__ISA_MIPS32__)
 # define nemu_trap(code) asm volatile ("move $v0, %0; sdbbp" : :"r"(code))
 #elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
-# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code)) // ebreak=>nemu_trap
 #elif defined(__ISA_LOONGARCH32R__)
 # define nemu_trap(code) asm volatile("move $a0, %0; break 0" : :"r"(code))
 #elif
