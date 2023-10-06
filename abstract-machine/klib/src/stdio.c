092886a2d73435450c9219e969815ef970035822
tracer-ics2022 <tracer@njuics.org>
2023-10-06 11:34:27 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:27 up 2 days, 20:40,  4 users,  load average: 0.18, 0.16, 0.16

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,14 +7,61 @@
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
+  //va_list ap;
+  //int d;
+  //char c;
+  //char *s;
+  //va_start(ap,word);
+  // const char fmt
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+int str_num(int n, char *s){
+  int len = 0;
+  int num = n;
+  for(;num>0;num/=10) len++;
+  for(int i=len ; i>0; i--) {
+	s[i-1] = n%10;
+	n /= 10;
+  }
+  return len; 
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  va_list ap;
+  char *buff = out;
+  int len = 0;
+  va_start(ap, fmt);
+  for(; *fmt!='\0'; fmt++){
+      if(*fmt != '%'){
+	  *buff = *fmt;
+	   buff ++;
+	   len++;
+      }
+      else{
+	fmt ++;
+	switch(*fmt){
+	  case 's':
+		char *s = va_arg(ap, char *);
+		strcmp(buff, s);
+		len += strlen(s);
+		buff ++; 
+		break;
+	  case 'd':
+		int d = va_arg(ap, int);
+		len += str_num(d, buff); 
+		buff ++;
+		break;
+	} // switch
+      } // else
+  } // for
+  *buff = '\0';
+  va_end(ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
