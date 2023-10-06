bfd2932f2476bf30d5fca5a1e2902660998ab10a
tracer-ics2022 <tracer@njuics.org>
2023-10-06 16:42:47 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:47 up 3 days,  1:48,  4 users,  load average: 0.11, 0.15, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -25,6 +25,10 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 10
+#define IRING_BUF_SIZE 16 // 环形缓冲区大小
+#define IRING_BUF_PC_START_INDEX 3 // 存放指令信息的开始位置
+static char iringbuf[IRING_BUF_SIZE][128+IRING_BUF_PC_START_INDEX]; // 环形缓冲区 
+static size_t iringbuf_head = 0; // 当前指令占据的位置
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
@@ -60,11 +64,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
+  int ilen = s->snpc - s->pc;   // the length of this pc.
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
-    p += snprintf(p, 4, " %02x", inst[i]);
+    p += snprintf(p, 4, " %02x", inst[i]);   // 每字节按照两位十六进制输出
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
@@ -72,6 +76,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
+  strcpy(iringbuf[iringbuf_head] + IRING_BUF_PC_START_INDEX, s->logbuf);
+  iringbuf_head = (iringbuf_head + 1)% IRING_BUF_SIZE;
 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
@@ -84,6 +90,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 }
 
 static void execute(uint64_t n) {
+  
   Decode s;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
@@ -109,8 +116,29 @@ void assert_fail_msg() {
   statistic();
 }
 
+void print_iring_buf(){
+  const char point[IRING_BUF_PC_START_INDEX] = "-->";
+  for(int i = 0; i < IRING_BUF_SIZE; ++i){
+	if((iringbuf[i][IRING_BUF_PC_START_INDEX] == '\0')) break;
+	if(((i+1) % IRING_BUF_SIZE) == iringbuf_head){
+	   strncpy(iringbuf[i], point, IRING_BUF_PC_START_INDEX);
+	}
+#ifdef CONFIG_ITRACE_COND
+	if(ITRACE_COND) { log_write("%s\n", iringbuf[i]); }
+#endif
+	printf("%s\n", iringbuf[i]);
+  }
+  return;
+}
+
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
+  // initialize iringbuf
+  iringbuf_head = 0;
+  for (int i = 0; i < IRING_BUF_SIZE; ++i) {
+	memset(iringbuf[i], ' ', IRING_BUF_PC_START_INDEX);  // 前几个位置初始
+	iringbuf[i][IRING_BUF_PC_START_INDEX] = '\0';
+  }
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
@@ -128,8 +156,10 @@ void cpu_exec(uint64_t n) {
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
-
     case NEMU_END: case NEMU_ABORT:
+     // if(nemu_state.state == NEMU_END && nemu_state.halt_ret != 0){
+	print_iring_buf();
+     // }
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
