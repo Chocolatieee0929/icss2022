f077f29b95ec50b8c89e4bf5a767ff876b13587f
tracer-ics2022 <tracer@njuics.org>
2023-10-07 15:56:43 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:43 up 45 min,  1 user,  load average: 0.06, 0.02, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -16,36 +16,53 @@
 #include <memory/host.h>
 #include <memory/paddr.h>
 #include <device/mmio.h>
+#include <generated/autoconf.h>
 #include <isa.h>
+#include <locale.h>
+
+
 
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
-#ifdef CONFIG_MTRACE
-#define MTRACE_SIZE 32
-static uint8_t mtrace[MTRACE_SIZE][CONFIG_MSIZE];
-static size_t mtrace_index = 0;
+#ifdef CONFIG_MTRACE_COND
+#define MTRACE_SIZE 128
+static char mtrace[MTRACE_SIZE];
+static const char mtrace_type[2][3] = {"Mw","Mr"};
 #endif
-
+#ifdef CONFIG_MTRACE
+static void memory_trace(paddr_t addr, int len, int data, int flag){
+  strncpy(mtrace, mtrace_type[flag], 2);
+  memset(mtrace + 2, ' ', 4);
+  char *p = mtrace + 6;
+  p += snprintf(p, sizeof(mtrace)-6, FMT_WORD "    ", addr);
+  p += snprintf(p, mtrace + sizeof(mtrace)-p, "%2d""    ", len);
+  uint8_t *inst = (uint8_t *)&data; 
+  for (int i = len - 1; i >= 0; i--) {
+	p += snprintf(p, 4, " %02x", inst[i]);
+  }
+  *p = '\0';
+#ifdef CONFIG_MTRACE_COND
+  // if(MTRACE_COND) {
+     log_write("%s\n", mtrace); 
+  //}
+#endif
+  puts(mtrace);
+}
+#endif
 
 // 将要访问的内存地址映射到pmem中的相应偏移位置
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len){
-#ifdef CONFIG_MTRACE
-  mtrace[mtrace_index++] = addr;
-#endif
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
-#ifdef CONFIG_MTRACE
-  mtrace[mtrace_index++] = addr; 
-#endif
   host_write(guest_to_host(addr), len, data);
 }
 
@@ -66,23 +83,31 @@ void init_mem() {
     p[i] = rand();
   }
 #endif
-#ifdef COMFIG_MTRACE
-  mtrace_index = 0;
-  for(int i = 0; i < MTRACE_SIZE; i++){
-	mtrace[i] = {};
-  }
-#endif
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+#ifdef CONFIG_MTRACE
+	word_t data = pmem_read(addr, len);
+	memory_trace(addr, len, data, 1);
+	return data;
+#else
+	return pmem_read(addr, len);
+#endif
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+#ifdef CONFIG_MTRACE
+	memory_trace(addr, len, -1, 1);
+#endif
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+	memory_trace(addr, len, data, 0);
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
