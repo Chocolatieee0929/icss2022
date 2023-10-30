1d02df2e39f56c20ec102574dcbc38f44edec9c8
tracer-ics2022 <tracer@njuics.org>
2023-10-30 16:20:39 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:39 up 2 days,  1:56,  3 users,  load average: 0.95, 0.52, 0.41

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -96,6 +96,9 @@ int Convert_FuncInfo(Elf32_Sym *elf_symtab, int symnum, Elf32_Shdr elfstrtab,FIL
 		// dedbug
 		printf("now:%ld ",index);
 		Elf32_Sym symentry = elf_symtab[index];
+		// debug
+		printf("symname: %x tfunc_start = 0x%x\t func_offset = 0x%x\n",
+			symentry.st_name, symentry.st_value, symentry.st_size);
 		fseek(fp, elfstrtab.sh_offset + symentry.st_name * sizeof(char), SEEK_SET);
 		assert(fscanf(fp, "%63s", Func[index].func_name));
 		Func[index].func_start = symentry.st_value;
