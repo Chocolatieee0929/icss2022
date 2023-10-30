485c9cf6cc923a531c8c0f23075a50f572beb15d
tracer-ics2022 <tracer@njuics.org>
2023-10-30 17:09:17 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:17 up 2 days,  2:45,  3 users,  load average: 0.52, 0.44, 0.39

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -158,8 +158,8 @@ void read_elf_func(Elf32_Ehdr Ehdr, FILE *fp){
   // 从符号表中type为FUNC类型将其转换成我们定义的结构体，函数名需要根据字符串表进行读取
   assert(Convert_FuncInfo(elf_symtab, symnum, elf_strtab, fp)==0);
   // free(elf_symtab);
-  rewind(fp);
-  fclose(fp);
+  // rewind(fp);
+  // fclose(fp);
 }
 
 // 初始化形成elf文件
