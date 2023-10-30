ac32ff530ee845564e5e4907ca48726b3f810b6b
tracer-ics2022 <tracer@njuics.org>
2023-10-30 11:27:47 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:47 up 1 day, 21:03,  3 users,  load average: 0.42, 0.39, 0.37

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -54,6 +54,7 @@ static Elf32_Ehdr read_system_tab(FILE *fp){
 int read_elf_symtab(Elf32_Sym *elf_symtab, Elf32_Shdr sec_sym, FILE *fp) {
   // 读取章节表的对应是函数的条目
   int symnum = sec_sym.sh_size / sec_sym.sh_entsize;
+  printf("sh_size:%x sh_entsize:%x", sec_sym.sh_size, sec_sym.sh_entsize);
   if((sec_sym.sh_size % sec_sym.sh_entsize) != 0) return 0;
   Elf32_Sym elf_func[symnum];
   rewind(fp);
