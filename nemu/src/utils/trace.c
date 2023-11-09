8c4285a30291c6739cb06b75b2013dad2b442ba0
tracer-ics2022 <tracer@njuics.org>
2023-11-09 15:56:27 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:27 up 6 days, 8 min,  2 users,  load average: 0.39, 0.40, 0.38

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -12,8 +12,8 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
-
-//#ifdef CONFIG_FTRACE
+/*
+#ifdef CONFIG_FTRACE
 #include <elf.h>
 #include <assert.h>
 #include <string.h>
@@ -32,8 +32,11 @@ typedef struct{
 FuncInfo Func[Func_num];
 void init_elf(const char *elf_file);
 
-//#endif
+#endif
+*/
 
+#ifdef CONFIG_FTRACE
+#include <trace.h>
 
 FILE *elf_fp = NULL;
 
@@ -196,3 +199,4 @@ void init_elf(const char *elf_file) {
   rewind(fp);
   fclose(fp);
 }
+#endif
