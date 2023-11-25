e7a1af7c1d7fb8eea2a131c040fe99581918d0a7
tracer-ics2022 <tracer@njuics.org>
2023-11-25 16:10:05 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:05 up 2 days, 14:45,  5 users,  load average: 0.33, 0.34, 0.30

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,36 +6,94 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
-  //va_list ap;
-  //int d;
-  //char c;
-  //char *s;
-  //va_start(ap,word);
-  // const char fmt
+  //panic("Not implemented");
+  va_list args;
+  int n;
+  char s[128];
+  // putch('Y');
+  va_start(args,fmt);
+  n = vsprintf(s,fmt,args);
+  va_end(args);
+  return n;
+}
+
+int numlen(int num){
+  int len = 0;
+  for(;num>0;num/=10) len++;
+  return len; 
+}
+
+void str_num(int n, char* buff){
+  int len = numlen(n);
+  char s[len];
+  for(int i = len-1;i>=0;i--){
+    int temp = n%10;
+    s[i]='0'+temp;
+    n/=10;
+  }
+  strcpy(buff,s);
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int str_num(int n, char *s){
+  // panic("Not implemented");
+  char *buff = out;
   int len = 0;
-  int num = n;
-  for(;num>0;num/=10) len++;
-  for(int i=len ; i>0; i--) {
-	s[i-1] = n%10;
-	n /= 10;
-  }
-  return len; 
+  //va_start(ap, fmt);
+  for(; *fmt!='\0'; fmt++){
+      if(*fmt != '%'){
+	  *buff = *fmt;
+	   buff ++;
+	   len++;
+	   // debug
+	   // putch('Y');
+      }
+      else{
+	fmt ++;
+	switch(*fmt){
+	  case 's':
+		char *s = va_arg(ap, char *);
+		if(s==NULL) s="<NULL>";
+		strcpy(buff, s);
+		// putch(*buff);
+		// putch('\n');
+		printf(buff);
+		len += strlen(s);
+		buff += strlen(s); 
+		break;
+	  case 'd':
+		int d = va_arg(ap, int);
+		int dlen = numlen(d); 
+		str_num(d,buff);
+		//strcpy(buff,num);
+		len += dlen;
+		//putch(*buff);
+		buff += dlen;
+		break;
+	  case 'c':
+		char c = va_arg(ap, int);
+		*buff = c;
+		len ++;
+		buff ++;
+		break;
+	} // switch
+      } // else
+  } // for
+  *buff = '\0';
+  //va_end(ap);
+  return len;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   // panic("Not implemented");
   va_list ap;
-  char *buff = out;
-  int len = 0;
+  //char *buff = out;
+  int n = 0;
   va_start(ap, fmt);
+  n = vsprintf(out,fmt,ap);
+  //printf("n:%d",n);
+  va_end(ap);
+  return n;
+  /*
   for(; *fmt!='\0'; fmt++){
       if(*fmt != '%'){
 	  *buff = *fmt;
@@ -47,6 +105,7 @@ int sprintf(char *out, const char *fmt, ...) {
 	switch(*fmt){
 	  case 's':
 		char *s = va_arg(ap, char *);
+		if(s==NULL) s="<NULL>";
 		strcmp(buff, s);
 		len += strlen(s);
 		buff ++; 
@@ -56,12 +115,19 @@ int sprintf(char *out, const char *fmt, ...) {
 		len += str_num(d, buff); 
 		buff ++;
 		break;
+	  case 'c':
+		char c = va_arg(ap, int);
+		*buff = c;
+		len ++;
+		buff ++;
+		break;
 	} // switch
       } // else
   } // for
   *buff = '\0';
   va_end(ap);
   return len;
+  */
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -7,48 +7,71 @@
 size_t strlen(const char *s) {
  //  panic("Not implemented");
   size_t len = 0;
+  if(s == NULL) return len;
   while(s[len] != '\0') len++;
+  // putch('Y');
+  // putch('\n');
   return len;
 }
 
 char *strcpy(char *dst, const char *src) {
   // panic("Not implemented");
+  if(src==NULL) return NULL;
   size_t len = strlen(src);
   for(size_t i = 0; i < len;i++) dst[i]=src[i];
   dst[len] = '\0';
+  // putch('Y');
+  // putch('\n');
   return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
   // panic("Not implemented");
+  putch('Y');
   size_t i;
-  for(i =0; i < n && src[i]!='\0'; i++){
-  	dst[i] = src[i];
+  if(src==NULL || dst==NULL) return NULL;
+  char *ans = dst;
+  for(i =0; i < n && *src !='\0'; i++){
+  	*dst = *src;
+	dst++;src++;
   }
-  for( ; i < n; i++) 
-	dst[i] = '\0';
-  return dst;
+  while(i!=n){
+	*dst = '\n';
+	dst++;i++;
+  }
+  putch('Y'); 
+  putch('\n');
+  return ans;
 }
 
 char *strcat(char *dst, const char *src) {
   // panic("Not implemented");
   size_t dst_len = strlen(dst);
+  size_t src_len = strlen(src);
   size_t i;
-  for (i = 0 ; i < dst_len ; i++)
-	  dst[dst_len + i] = src[i];
-  dst[dst_len + i] = '\0';
-  return dst;
+  char *ans = dst;
+  dst += dst_len;
+  for (i = 0 ; i < src_len ; i++){
+	  *dst = *src;
+	  dst++;src++;
+  }
+  *dst = '\0';
+  return ans;
 }
 
 int strcmp(const char *s1, const char *s2) {
   // panic("Not implemented");
+  if(!(s1||s2)) return 0;
+  else if(s1==NULL) return 1;
+  else if(s2==NULL) return -1;
   size_t i = 0;
-  for(; s1[i]!='\0' && s2[i]!='\0';i++){
-  	if(s1[i] < s2[i]) return -1;
-	else if(s1[i] > s2[i]) return 1;
+  for(; *s1!='\0' && *s2!='\0';i++){
+  	if(*s1 < *s2) return -1;
+	else if(*s1 > *s2) return 1;
+	s1++;s2++;
   }
-  if(s1[i] == s2[i]) return 0;
-  else if(s1[i]=='\0') return -1;
+  if(*s1 == *s2) return 0;
+  else if(*s1=='\0') return -1;
   return 1;
 }
 
