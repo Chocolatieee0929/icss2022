43f10b3f7c3e8b9efc622f83df8d272be19ceb68
tracer-ics2022 <tracer@njuics.org>
2023-11-26 18:16:08 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:08 up 2 days, 23:18,  7 users,  load average: 0.62, 0.45, 0.37

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -116,12 +116,12 @@ void func_trace(Decode *s,paddr_t target){
 	}
 	temp->func_type = func_r;
 	// debug
-	printf(" ret [%s@0x%x]\n",Func[to].func_name, Func[to].func_start);
+	//printf(" ret [%s@0x%x]\n",Func[to].func_name, Func[to].func_start);
   }
   else {
 	temp->func_type = func_c;
 	// debug
-	printf(" call [%s@0x%x]\n",Func[to].func_name, Func[to].func_start);
+	//printf(" call [%s@0x%x]\n",Func[to].func_name, Func[to].func_start);
   }
   temp->next = NULL;
   fend->next=temp; fend = temp; 
