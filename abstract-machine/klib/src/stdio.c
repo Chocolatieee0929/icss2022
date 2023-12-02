2b3f1f55adab2fc9c3121220b65ea75a2e769dfd
tracer-ics2022 <tracer@njuics.org>
2023-12-02 20:26:20 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:20 up 4 days, 18:56, 11 users,  load average: 1.83, 1.66, 1.58

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -11,6 +11,24 @@
 #define SPECIAL	32		/* 0x /0*/
 #define LARGE	64		/* 用 'ABCDEF'/'abcdef' */
 
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+int printf(const char *fmt, ...) {
+  //panic("Not implemented");
+  va_list args;
+  int n;
+  char s[128];
+  // putch('Y');
+  va_start(args,fmt);
+  n = vsprintf(s,fmt,args);
+  va_end(args);
+  for(int i=0; i<128 && s[i]!='\0'; i++){
+	putch(s[i]);
+  }
+  return n;
+}
+
 //统计字符串中字符个数(不包括\0)，如果个数大于count，则返回count，否则返回字符个数
 size_t strnlen(const char * s, size_t count)//size_t是unsigned long long的宏
 {
@@ -94,27 +112,22 @@ static char * number(char * str, unsigned long long num, int base, int size, int
 	c = (type & ZEROPAD) ? '0' : ' ';//如果标志符有0则补0，否则补空格；例如%02d
 	sign = 0;//符号
 	
-	if (type & SIGN) //有符号与无符号的转换
-    {
-		if ((signed long long)num < 0) 
-        {
+	if (type & SIGN) {//有符号与无符号的转换
+		if ((signed long long)num < 0) {
 			sign = '-';
 			num = - (signed long long)num;//取正值
 			size--;//字段宽度减1
-		} else if (type & PLUS) //显示+
-        {
+		} else if (type & PLUS) {//显示+
 			sign = '+';
 			size--;
-		} else if (type & SPACE)//填补空格
-        {
+		} else if (type & SPACE){//填补空格
 			sign = ' ';
 			size--;
 		}
 	}
 	
     //处理十六进制字宽问题
-	if (type & SPECIAL) //十六进制显示
-    {
+	if (type & SPECIAL){ //十六进制显示
 		if (base == 16)
 			size -= 2;//0x
 		else if (base == 8)
@@ -124,8 +137,7 @@ static char * number(char * str, unsigned long long num, int base, int size, int
 	i = 0;
 	if (num == 0)//如果参数为0，则记录字符0
 		tmp[i++]='0'; //tmp中的内容会放到缓冲区中
-	else while (num != 0) //循环,num /= base
-    {
+	else while (num != 0){ //循环,num /= base
 		tmp[i++] = digits[do_div(num, base)];//将进制转换,低地址先进tmp？
 	}
     //地址长度大于精度，直接按地址长度输出，如果精度大于地址位数，先补空格
@@ -139,12 +151,10 @@ static char * number(char * str, unsigned long long num, int base, int size, int
 	if (sign)//如果有符号，输出符号，符号包括：'-','+','',0
 		*str++ = sign;
     
-	if (type & SPECIAL) //输出8进制或16进制符号0或0x
-    {
+	if (type & SPECIAL){ //输出8进制或16进制符号0或0x
 		if (base==8)
 			*str++ = '0';
-		else if (base==16) 
-        {
+		else if (base==16){
 			*str++ = '0';
 			*str++ = digits[33];//x或X
 		}
@@ -162,8 +172,7 @@ static char * number(char * str, unsigned long long num, int base, int size, int
 	return str;
 }
 
-int vsprintf(char *buf, const char *fmt, va_list args)
-{
+int vsprintf(char *buf, const char *fmt, va_list args){
 	int len;
 	unsigned long long num;
 	int i, base;
@@ -181,21 +190,17 @@ int vsprintf(char *buf, const char *fmt, va_list args)
     
     
 	/*将字符逐个放到输出缓冲区中，直到遇到第一个%*/
-	for (str=buf ; *fmt ; ++fmt) 
-    {
-		if (*fmt != '%') 
-        {    //寻找%
+	for (str=buf ; *fmt ; ++fmt) {
+		if (*fmt != '%') {    //寻找%
 			*str++ = *fmt;
 			continue;
 		}
-        
 		//遇到%后执行下面代码	
 		/* process flags */
 		flags = 0;
 		repeat:
 			++fmt;				//跳过第一个 '%'
-			switch (*fmt) 		//判断%后面的字符，对格式运算符的标志的处理
-            {		
+			switch (*fmt){ 		//判断%后面的字符，对格式运算符的标志的处理		
 				case '-': flags |= LEFT; goto repeat;//flags=10000(二进制，下面一样)
 				case '+': flags |= PLUS; goto repeat;//flags=100
 				case ' ': flags |= SPACE; goto repeat;//flags=1000
@@ -206,16 +211,15 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 		field_width = -1;
 		if ('0' <= *fmt && *fmt <= '9')
 			field_width = skip_atoi(&fmt);  //得到字段宽度
-		else if (*fmt == '*')               //*表示可变宽度
-        {             
+		else if (*fmt == '*'){              //*表示可变宽度
 			++fmt;
 			field_width = va_arg(args, int);//获得表示字段宽度的参数，
             
 			/*一般使用最后一个固定参数args来初始化这个函数，
 			得到的下一个参数为第一个变参,即printf("%*d",a,b);中的a,
 			这里a表示字段宽度。字符串为固定参数；变参函数至少要有一个固定参数。*/
-			if (field_width < 0) //手动输入负数，左对齐，例如printf("%*d",-2,3);
-            {	
+			if (field_width < 0){ //手动输入负数，左对齐，例如printf("%*d",-2,3);
+            	
 				field_width = -field_width;
 				flags |= LEFT;
 			}
@@ -223,13 +227,11 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 
 		// 获取精度 
 		precision = -1;
-		if (*fmt == '.') 
-        {
+		if (*fmt == '.'){
 			++fmt;	
 			if ('0' <= *fmt && *fmt <= '9')
 				precision = skip_atoi(&fmt);//获得精度
-			else if (*fmt == '*') //可变精度，printf("%.*f",2,3.1415);-->3.14
-            {
+			else if (*fmt == '*'){ //可变精度，printf("%.*f",2,3.1415);-->3.14
 				++fmt;
 				/* 获取表示精度的参数(以整数类型获取) */
 				precision = va_arg(args, int);
@@ -240,13 +242,11 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 	
 		//获取转换修饰符,即%hd、%ld、%lld、%Lf...中的h、l、L、Z (ll用q代替)
 		qualifier = -1;
-		if (*fmt == 'l' && *(fmt + 1) == 'l') 
-        {
-			qualifier = 'q';//即ll
+		if (*fmt == 'l' && *(fmt + 1) == 'l'){ 
+        		qualifier = 'q';//即ll
 			fmt += 2;
 		} else if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L'
-			|| *fmt == 'Z') 
-        {
+			|| *fmt == 'Z'){
 			qualifier = *fmt;
 			++fmt;
 		}
@@ -254,16 +254,14 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 		
 		base = 10;//默认十进制
 		//对c、s、p、n、%、o等做处理
-		switch (*fmt) 
-        {
+		switch (*fmt) {
 		//转换格式符为%c
 		case 'c':
 	            //如果没有有‘-’，先输出字宽-1个空格再输出字符
 			if (!(flags & LEFT))//如果没有'-'标记符
 				while (--field_width > 0)
 					*str++ = ' ';//根据字段输出空格' '
-            /*获取字符参数时是先以int类型获取再强转为unsigned char，
-                为了获取过程中保证精度不丢失。*/
+            /*获取字符参数时是先以int类型获取再强转为unsigned char，为了获取过程中保证精度不丢失。*/
 			*str++ = (unsigned char) va_arg(args, int);
 	        // 如果有'-'，先输出字符再填补空格,注意是先--的，所以实际空格会比输入的字段少1，在加					上参数就刚好够宽度；比如printf("%5d",2);输出：空格空格空格空格2。
 			while (--field_width > 0)
@@ -292,18 +290,17 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 	
 	    //处理格式符%p       
 		case 'p':
-			if (field_width == -1) //如果没有设置字段宽度
-            { 
-                /*字宽为8或16(根据系统而定)，因为2个位表示一个直接；
-                例如32位系统指针大小位4字节，oxFF FF FF FF,需要8个字宽才能存储*/
-				field_width = 2*sizeof(void *);
-				flags |= ZEROPAD;   //flags = 1;会在前面补0
-			}
-	        //转为16进制并存进缓冲区中
-			str = number(str,
-				(unsigned long) va_arg(args, void *), 16,
-				field_width, precision, flags);
-			continue;
+			if (field_width == -1){  //如果没有设置字段宽度
+      
+       		 /*字宽为8或16(根据系统而定)，因为2个位表示一个直接；
+        		例如32位系统指针大小位4字节，oxFF FF FF FF,需要8个字宽才能存储*/
+       			 	field_width = 2*sizeof(void *);
+        			flags |= ZEROPAD;   //flags = 1;会在前面补0
+      			}
+      //转为16进制并存进缓冲区中
+      			str = number(str, (unsigned long) va_arg(args, void *), 16, field_width, precision, flags);
+      			continue;
+
 	
 		//buf为1024字节空间的输出缓冲区（静态char数组）
 		case 'n':
@@ -348,31 +345,27 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 				--fmt;
 			continue;
 		}
-		if (qualifier == 'l') 
-        {
+		if (qualifier == 'l') {
 			num = va_arg(args, unsigned long);
 			if (flags & SIGN)
 				num = (signed long) num;
-		} else if (qualifier == 'q') 
-        {
+		} else if (qualifier == 'q') {
 			num = va_arg(args, unsigned long long);
 			if (flags & SIGN)
 				num = (signed long long) num;
-		} else if (qualifier == 'Z') 
-        {
+		} else if (qualifier == 'Z') {
 			num = va_arg(args, size_t);
-		} else if (qualifier == 'h') 
-        {
+		} else if (qualifier == 'h') {
+        
             //输出短整型时是先以整数来获取参数，再转为短整型输出，保证获取过程中精度不丢失
 			num = (unsigned short) va_arg(args, int);
 			if (flags & SIGN)
 				num = (signed short) num;
-		} else 
-        {
+		} 
+		else{ 
             //没有特殊标志的格式符，一律先以无符号整型获取，再转为有符号整型
 			num = va_arg(args, unsigned int);
-			if (flags & SIGN)
-				num = (signed int) num;
+			if (flags & SIGN) num = (signed int) num;
 		}
         //转换为对应的个数再存到缓冲区中
 		str = number(str, num, base, field_width, precision, flags);
@@ -380,3 +373,13 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 	*str = '\0';//最后以'\0'结束
 	return str-buf;
 }
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  panic("Not implemented");
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+#endif
