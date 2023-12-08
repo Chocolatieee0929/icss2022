db9c680cff3cd8e6542bab6dd8e206e71d589355
tracer-ics2022 <tracer@njuics.org>
2023-12-08 13:41:50 +0800

    >  compile NEMU 2112233094 Renata Luo Linux renata-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:41:50 up 10:44,  5 users,  load average: 0.17, 0.22, 0.25

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,7 +35,20 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+ if(pre_addr == NULL){
+	pre_addr = (void *)ROUNDUP(heap.start, 8);
+ }
+ size  = (size_t)ROUNDUP(size, 8);
+ char *res = pre_addr;
+ res += size;
+ assert((uintptr_t)heap.start <= (uintptr_t)pre_addr && (uintptr_t)pre_addr < (uintptr_t)heap.end);
+ uint64_t *p = (uint64_t *)pre_addr;
+ while( p != (uint64_t *)res ){
+ // init this
+ *p = 0;
+ p++;
+ }
+ return res; 
 #endif
   return NULL;
 }
